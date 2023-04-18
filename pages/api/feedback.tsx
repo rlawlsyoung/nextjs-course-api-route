19b2b90065174b778afca3a3dc7292358ba3c27e
김진영 <jinyoung01099@gmail.com>
2023-04-18 15:47:54 +0900

    [Add] feedback api post 구현

--- pages/api/feedback.tsx
+++ pages/api/feedback.tsx
@@ -1,5 +1,24 @@
+import fs from "fs";
+import path from "path";
+
 const handler = (req: any, res: any) => {
-  res.status(200).json({ message: "hi there" });
+  if (req.method === "POST") {
+    const email = req.body.email;
+    const feedback = req.body.text;
+
+    const newFeedback = {
+      id: new Date().toISOString(),
+      email: email,
+      text: feedback,
+    };
+
+    const filePath = path.join(process.cwd(), "data", "feedback.json");
+    const fileData = fs.readFileSync(filePath);
+    const data = JSON.parse(fileData.toString());
+    data.push(newFeedback);
+    fs.writeFileSync(filePath, JSON.stringify(data));
+    res.status(201).json({ message: "success!", feedback: newFeedback });
+  } else res.status(200).json({ message: "hi there" });
 };
 
 export default handler;
